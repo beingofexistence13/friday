b66bad41ec0e18305b651f07fd4c373aac54c889
Man-from-earth25 <manfromearth25@gmail.com>
2023-09-15 00:27:57 +0600

    feat: i am the best

--- bard.py
+++ bard.py
@@ -17,13 +17,40 @@ import uuid
 import pyperclip
 import pandas as pd
 warnings.simplefilter("ignore")
-# pip install bardapi datetime
 
+# Specify the filename
+filename_json = 'bardCookie.json'
+
+# Read the content of the file
+with open(os.path.join('database', filename_json), 'r') as f:
+    json_data = json.load(f)
+
+SID = "__Secure-1PSID"
+TS = "__Secure-1PSIDTS"
+CC = "__Secure-1PSIDCC"
+SIDValue = next((item for item in json_data if item["name"] == SID), None)
+TSValue = next((item for item in json_data if item["name"] == TS), None)
+CCValue = next((item for item in json_data if item["name"] == CC), None)
+if SIDValue is not None:
+    SIDValue = SIDValue["value"]
+else:
+    print(f"{SID} not found in the JSON data.")
+
+if TSValue is not None:
+    TSValue = TSValue["value"]
+else:
+    print(f"{TS} not found in the JSON data.")
+
+if CCValue is not None:
+    CCValue = CCValue["value"]
+else:
+    print(f"{CC} not found in the JSON data.")
 cookie_dict = {
-    "__Secure-1PSID":"awjpc78-pBVnngqICnsMnl49b3IifcS23WOl8FjnnyhOKOhumLha7wDR2KkT9o8VJNeYBA.",
-    "__Secure-1PSIDTS":"sidts-CjIBSAxbGaMg6qpTVegRSnnQ5Ajjr085UW9B_CP2hRG45ssmD3040Eaz0kmiPITM_LA93RAA",
-    "__Secure-1PSIDCC":"APoG2W855-2gQfY7mpjEx7AAmRNC9vAxDBSA5nXwfGTdOdZ1Z5vPNvmrANUKsscGNeuGNpL0"
+    "__Secure-1PSID": SIDValue,
+    "__Secure-1PSIDTS": TSValue,
+    "__Secure-1PSIDCC": CCValue,
 }
+
 def CookieScrapper():
     webbrowser.open("https://bard.google.com")
     sleep(2)
@@ -33,57 +60,26 @@ def CookieScrapper():
     sleep(1)
     keyboard.press_and_release('ctrl + w')
 
-    # # Get data from clipboard
-    # data = pyperclip.paste()
-
-    # # Generate a unique filename
-    # filename = f'cookies_{uuid.uuid4().hex}.txt'
-
-    # # Create 'database' directory if it doesn't exist
-    # if not os.path.exists('database'):
-    #     os.makedirs('database')
-
-    # # Create the unique file in the 'database' directory
-    # with open(os.path.join('database', filename), 'w') as f:
-    #     f.write(data)
-
-    # print(f'Data written to file: {filename}')
-    
-    # # Get data from clipboard
-    # data = pyperclip.paste()
-
-    # # Generate a unique filename
-    # filename_csv = f'cookies_{uuid.uuid4().hex}.csv'
-
-    # # Create 'database' directory if it doesn't exist
-    # if not os.path.exists('database'):
-    #     os.makedirs('database')
-
-    # # Assuming that the clipboard data is tab-separated, read the data into a DataFrame
-    # df = pd.read_csv(io.StringIO(data), sep='\t')
-
-    # # Write the DataFrame to a CSV file in the 'database' directory
-    # df.to_csv(os.path.join('database', filename_csv), index=False)
-
-    # print(f'Data written to file: {filename_csv}')
-
-
     # Get data from clipboard
     data = pyperclip.paste()
 
-    # Generate a unique filename
-    filename_json = f'cookies_{uuid.uuid4().hex}.json'
+    # Specify the filename
+    filename_json = 'bardCookie.json'
 
     # Create 'database' directory if it doesn't exist
     if not os.path.exists('database'):
         os.makedirs('database')
 
-    # Create the unique file in the 'database' directory and write data to it
+    # Create or overwrite the file in the 'database' directory and write data to it
     with open(os.path.join('database', filename_json), 'w') as f:
         # Convert string data to JSON format and write to file
         json.dump(json.loads(data), f)
 
-    print(f'Data written to file: {filename_json}')
+    # Read the content of the file and print it
+    with open(os.path.join('database', filename_json), 'r') as f:
+        content = json.load(f)
+        print(content)
+
     try:
         json_data = json.loads(data)
         pass
@@ -123,7 +119,6 @@ def CookieScrapper():
 
     return cookie_dict
 
-
 try:
     bard = BardCookies(cookie_dict=cookie_dict)
 except Exception as e:
@@ -131,7 +126,7 @@ except Exception as e:
     bard = BardCookies(cookie_dict=cookie_dict)
     print("Yes, there is a cookie error:", str(e),"And Using CookieScrapper!!!")
 else:
-    print("NO, no no.. there are no errors :) All Set To Your Query")
+    print("")
 
 # Text Modification Function -
 def split_and_save_paragraphs(data, filename):
