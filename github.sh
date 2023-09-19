bfe0fc4e66d3c269656a3eb88e55b05e18ff76bf
Man-from-earth25 <manfromearth25@gmail.com>
2023-09-20 00:39:36 +0600

    feat: hello

---
+++ github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Create a file named truth.txt
+touch truth.txt && echo "Alhamdulillah" > truth.txt && git add . && git commit -m "feat: hello" && git push && rm -rf truth.txt && git add . && git commit -m "feat: hello" && git push
+
