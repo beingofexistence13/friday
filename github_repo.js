473501156828a8299d61c08345c96d3c8b31aec5
Man-from-earth25 <manfromearth25@gmail.com>
2023-09-28 12:50:55 +0600

    feat: ssc truama boring study, haah let get ready for HSC sumon

--- github_repo.js
+++ github_repo.js
@@ -7,16 +7,21 @@ async function getRepos(username, token) {
     };
   
     let repos = [];
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 6; i++) {
       const response = await fetch(`https://api.github.com/users/${username}/repos?type=all&per_page=100&page=${i}`, { headers });
       const data = await response.json();
       repos = repos.concat(data);
     }
   
     repos.forEach((repo) => {
-      console.log(`Name:${repo.name},Description:${repo.description}`);
+      console.log(`
+      {
+        name:"${repo.name}",
+        description:"${repo.description}"
+      },
+      `);
     });
   }
   
 
-getRepos('beingofexistence', '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'); // replace 'username' with the GitHub username and 'your_token' with your personal access token
+getRepos('beingofexistence', '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');
